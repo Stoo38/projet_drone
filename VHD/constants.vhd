a6418f062908319590f9dbcec54d64eca8cbf9b1
Stoo38 <b.mahtal@gmail.com>
2017-12-12 23:57:02 +0100

    Mise à jour du fichier de constantes

--- VHD/constants.vhd
+++ VHD/constants.vhd
@@ -7,16 +7,22 @@ use ieee.numeric_std.all;
 
 package constants_package is 
 
-	constant const_vector_size				: integer := 23; -- taille des vecteurs de bits 
+	constant const_vector_size  			: integer := 23; -- taille des vecteurs de bits 
 
-	constant const_clk_period				: std_logic_vector(5 downto 0) := "101000";						-- ns (période de la clock)
-	constant const_tempo 					: std_logic_vector(const_vector_size - 1 downto 0) := "00001001001001111100000";	-- ns (durée d'une temporisation)
-	constant const_t_min 					: std_logic_vector(const_vector_size - 1 downto 0) := "00010101010111001100000";	-- ns (durée de la trame la plus courte)
-	constant const_t_max 					: std_logic_vector(const_vector_size - 1 downto 0) := "00110011111000010100000";				-- ns (durée de la trame la plus longue)
-	constant const_cycles_tempo				: std_logic_vector(const_vector_size - 1 downto 0) := "00110011111000010100000";integer := tempo/clk_period;		-- Nombre de cycles nécessaires pour la temporisation
-	constant const_cycles_trame_min			: std_logic_vector(const_vector_size - 1 downto 0) := "00110011111000010100000";integer := t_min/clk_period; 		-- Nombre de cycles nécessaires pour la trame la plus courte
-	constant const_nb_cycles_trame_max		: integer := t_max/clk_period;		-- Nombre de cycles nécessaires pour la trame la plus longue
-	constant const_pas						: integer := 4000;					-- valeur du pas de quantification
+	constant const_clk_period					: std_logic_vector(5 downto 0) := "101000";						-- ns (période de la clock)
+	constant const_tempo							: std_logic_vector(const_vector_size - 1 downto 0) := "00001001001001111100000";		-- ns (durée d'une temporisation)
+	constant const_t_min							: std_logic_vector(const_vector_size - 1 downto 0) := "00010101010111001100000";		-- ns (durée de la trame la plus courte)
+	constant const_t_max							: std_logic_vector(const_vector_size - 1 downto 0) := "00110011111000010100000";		-- ns (durée de la trame la plus longue)
+	constant const_cycles_tempo				: std_logic_vector(const_vector_size - 1 downto 0) := "00000000001110101001100";		-- Nombre de cycles nécessaires pour la temporisation
+	constant const_cycles_trame_min		: std_logic_vector(const_vector_size - 1 downto 0) := "00000000100010001011100";		-- Nombre de cycles nécessaires pour la trame la plus courte
+	constant const_cycles_trame_max		: std_logic_vector(const_vector_size - 1 downto 0) := "00000001010011000000100";		-- Nombre de cycles nécessaires pour la trame la plus longue
+  constant const_pas								: std_logic_vector(const_vector_size - 1 downto 0) := "00000000000111101010000";		-- valeur du pas de quantification
 
+	constant const_cycles_synchro_min	: std_logic_vector(const_vector_size - 1 downto 0) := "00001010110101010111000";
+	
+	constant const_cycles_synchro_max	: std_logic_vector(const_vector_size - 1 downto 0) := "00001101111000101011000";
+
+
+	constant const_test : std_logic_vector(const_vector_size - 1 downto 0) := std_logic_vector(unsigned(const_t_min)/unsigned(const_clk_period));	
 
 end package constants_package;
--- VHD/send_ppm.vhd
+++ VHD/send_ppm.vhd
@@ -12,11 +12,11 @@ use LIB_VHD.constants_package.all;
 entity send_ppm is
 	port 
 	(
-		i_clk		: in std_logic;
+		i_clk			: in std_logic;
 		i_reset_n	: in std_logic;
 		i_enable	: in std_logic;
 		i_reg   	: in std_logic_vector(31 downto 0); 
-		o_ppm		: out std_logic
+		o_ppm			: out std_logic
 	);
 
 end send_ppm;
@@ -25,54 +25,46 @@ architecture rtl of send_ppm is
 	
 	--Appel de compteur local et compteur global
 	component global_counter port (	i_clk  		: in  	std_logic;                      
-				               		i_reset_n	: in  	std_logic;                     
-				  					o_count 	: out 	std_logic_vector(19 downto 0));
+																	i_reset_n	: in  	std_logic;                     
+																	o_count 	: out 	std_logic_vector(19 downto 0));
 	end component;
 	component local_counter port(	i_clk		: in 	std_logic;
-						        	i_reset_n	: in 	std_logic;
-									i_init		: in 	std_logic;
-									o_count 	: out 	std_logic_vector(18 downto 0)); 
+																i_reset_n	: in 	std_logic;
+																i_init		: in 	std_logic;
+																o_count 	: out 	std_logic_vector(18 downto 0)); 
 	end component;
 
 	--Création du type pour définir les différents états
 	type ppm_states is ( 	synchro, 			
-							first_pulse,
-							second_pulse,
-							third_pulse,
-							fourth_pulse,
-							synchro_delay,
-							first_delay,
-							second_delay,
-							third_delay,
-							fourth_delay);
+												first_pulse,
+												second_pulse,
+												third_pulse,
+												fourth_pulse,
+												synchro_delay,
+												first_delay,
+												second_delay,
+												third_delay,
+												fourth_delay);
 
 	-- Signaux représentant les différents états
 	signal current_state, next_state : ppm_states;
 
 	--Signaux du bloc send_ppm
 	signal sig_in_clk     			: std_logic := '0';
-	signal sig_in_reset_n     		: std_logic;
-	signal sig_in_enable			: std_logic;
-	signal sig_in_reg				: std_logic_vector(31 downto 0);
-	signal sig_out_ppm				: std_logic;
+	signal sig_in_reset_n     	: std_logic;
+	signal sig_in_enable				: std_logic;
+	signal sig_in_reg						: std_logic_vector(31 downto 0);
+	signal sig_out_ppm					: std_logic;
 
 	--Signaux des compteurs 
-	signal sig_in_init				: std_logic;	
-	signal sig_out_local_count   	: std_logic_vector(18 downto 0);
-	signal sig_out_global_count   	: std_logic_vector(19 downto 0);
+	signal sig_in_init					: std_logic;	
+	signal sig_out_local_count  : std_logic_vector(18 downto 0);
+	signal sig_out_global_count : std_logic_vector(19 downto 0);
 
 	
 
 	--Déclaration des constantes
-	constant clk_period 			: integer := 40; 					-- ns (période de la clock)
-	constant tempo 					: integer := 300000; 				-- ns (durée d'une temporisation)
-	constant t_min 					: integer := 700000;				-- ns (durée de la trame la plus courte)
-	constant t_max 					: integer := 1700000;				-- ns (durée de la trame la plus longue)
-	constant nb_cycles_tempo 		: integer := tempo/clk_period;		-- Nombre de cycles nécessaires pour la temporisation
-	constant nb_cycles_trame_min 	: integer := t_min/clk_period; 		-- Nombre de cycles nécessaires pour la trame la plus courte
-	constant nb_cycles_trame_max 	: integer := t_max/clk_period;		-- Nombre de cycles nécessaires pour la trame la plus longue
-	constant pas					: integer := 4000;					-- valeur du pas de quantification
-
+	constant test_test				: std_logic_vector(const_vector_size - 1 downto 0) := std_logic_vector(unsigned(const_t_min)/unsigned(const_clk_period));			
 
  
 
